5107260095ace1f606f2dda3bc70413ae807c904
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 08:41:51 +0100

    Commit

---
+++ Greetings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Please enter a name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+
+            Console.WriteLine("Press Enter to restart, or type 'exit' to close:");
+            string command = Console.ReadLine();
+
+            if (command.ToLower() == "exit")
+            {
+                break;
+            }
+
+            Console.Clear();
+        }
+    }
+}
